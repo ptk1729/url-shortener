f83966a8d71932e76a1aa7db56bc333e233d2a36
Prateek Rohilla <prateekrohilla4.pr@gmail.com>
2024-09-13 14:07:40 +0530

    Refactor database configuration for production environment

--- config/config.js
+++ config/config.js
@@ -11,18 +11,14 @@ module.exports = {
       ssl: process.env.DB_SSL === 'true' ? { require: true } : false
     }
   },
-  test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-  },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    dialect: 'postgres', // or whatever dialect you are using
+    dialectOptions: {
+      ssl: process.env.DB_SSL === 'true' ? { require: true } : false
+    }
   }
 }
